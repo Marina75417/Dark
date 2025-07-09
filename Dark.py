75d6efef4523f9712a8c12a499b9e7baba1df853
Marinakhan <To.hi.to.mera.hamdard.hai@gmail.com>
2025-07-09 08:25:41 +0500

    Update Dark.py

--- Dark.py
+++ Dark.py
@@ -1309,7 +1309,7 @@ class Main:
 
 
 def Subscraption():
-	key1=open('/data/data/com.termux/files/usr/bin/.mrso4av -cov, r')
+	key1=open('/data/data/com.termux/files/usr/bin/marina -cov, r')
 	clear()
 	print(logo)
 	r1=requests.get("https://github.com/Marina75417").text
